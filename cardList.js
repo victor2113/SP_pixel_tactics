352f18113dc84a89ede6decb9f5b5a58744bce8a
victor2113 <80439643+victor2113@users.noreply.github.com>
2021-07-25 15:18:14 +1000

    Add files via upload

---
+++ cardList.js
@@ -0,0 +1,220 @@
+class passiveAbilityID {
+    constructor() {
+
+    }
+}
+class spellID {
+    constructor() {
+
+    }
+}
+
+class vanguardAbilityId {
+
+}
+class flankAbilityId {
+
+}
+class rearAbilityId {
+
+}
+class orderId {
+
+}
+
+class berserk extends Card {
+    constructor() {
+        super(8, 3, vanguardAbilityId, flankAbilityId, rearAbilityId, orderId);
+    }
+}
+
+class stalker extends Card {
+    constructor() {
+        super(7, 3, vanguardAbilityId, flankAbilityId, rearAbilityId, orderId);
+    }
+}
+
+class scientist extends Card {
+    constructor() {
+        super(5, 2, vanguardAbilityId, flankAbilityId, rearAbilityId, orderId);
+    }
+}
+
+class paladin extends Card {
+    constructor() {
+        super(6, 4, vanguardAbilityId, flankAbilityId, rearAbilityId, orderId);
+    }
+}
+
+class healer extends Card {
+    constructor() {
+        super(4, 1, vanguardAbilityId, flankAbilityId, rearAbilityId, orderId);
+    }
+}
+
+class gunner extends Card {
+    constructor() {
+        super(4, 5, vanguardAbilityId, flankAbilityId, rearAbilityId, orderId);
+    }
+}
+
+class warrior extends Card {
+    constructor() {
+        super(8, 4, vanguardAbilityId, flankAbilityId, rearAbilityId, orderId);
+    }
+}
+
+class homunculus extends Card {
+    constructor() {
+        super(7, 3, vanguardAbilityId, flankAbilityId, rearAbilityId, orderId);
+    }
+}
+
+class talisman extends Card {
+    constructor() {
+        super(4, 1, vanguardAbilityId, flankAbilityId, rearAbilityId, orderId);
+    }
+}
+
+class witch extends Card {
+    constructor() {
+        super(5, 1, vanguardAbilityId, flankAbilityId, rearAbilityId, orderId);
+    }
+}
+
+class huntsman extends Card {
+    constructor() {
+        super(5, 4, vanguardAbilityId, flankAbilityId, rearAbilityId, orderId);
+    }
+}
+
+class lord extends Card {
+    constructor() {
+        super(7, 3, vanguardAbilityId, flankAbilityId, rearAbilityId, orderId);
+    }
+}
+class mystic extends Card {
+    constructor() {
+        super(7, 1, vanguardAbilityId, flankAbilityId, rearAbilityId, orderId);
+    }
+}
+
+class pyromancer extends Card {
+    constructor() {
+        super(3, 3, vanguardAbilityId, flankAbilityId, rearAbilityId, orderId);
+    }
+}
+
+class priest extends Card {
+    constructor() {
+        super(7, 1, vanguardAbilityId, flankAbilityId, rearAbilityId, orderId);
+    }
+}
+
+class dragon_mage extends Card {
+    constructor() {
+        super(3, 6, vanguardAbilityId, flankAbilityId, rearAbilityId, orderId);
+    }
+}
+
+class knight extends Card {
+    constructor() {
+        super(10, 3, vanguardAbilityId, flankAbilityId, rearAbilityId, orderId);
+    }
+}
+
+class illusionist extends Card {
+    constructor() {
+        super(5, 4, vanguardAbilityId, flankAbilityId, rearAbilityId, orderId);
+    }
+}
+
+class vampire extends Card {
+    constructor() {
+        super(6, 2, vanguardAbilityId, flankAbilityId, rearAbilityId, orderId);
+    }
+}
+
+class oracle extends Card {
+    constructor() {
+        super(4, 22, vanguardAbilityId, flankAbilityId, rearAbilityId, orderId);
+    }
+}
+
+class alchemist extends Card {
+    constructor() {
+        super(4, 4, vanguardAbilityId, flankAbilityId, rearAbilityId, orderId);
+    }
+}
+
+class assassin extends Card {
+    constructor() {
+        super(1, 1, vanguardAbilityId, flankAbilityId, rearAbilityId, orderId);
+    }
+}
+class double extends Card {
+    constructor() {
+        super(5, 5, vanguardAbilityId, flankAbilityId, rearAbilityId, orderId);
+    }
+}
+
+class templar extends Card {
+    constructor() {
+        super(6, 1, vanguardAbilityId, flankAbilityId, rearAbilityId, orderId);
+    }
+}
+
+class summoner extends Card {
+    constructor() {
+        super(5, 2, vanguardAbilityId, flankAbilityId, rearAbilityId, orderId);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+cards = [
+    new berserk(),
+    new stalker(),
+    new scientist(),
+    new paladin(),
+    new healer(),
+    new gunner(),
+    new warrior(),
+    new homunculus(),
+    new talisman(),
+    new witch(),
+    new huntsman(),
+    new lord(),
+    new mystic(),
+    new pyromancer(),
+    new priest(),
+    new dragon_mage(),
+    new knight(),
+    new illusionist(),
+    new vampire(),
+    new oracle(),
+    new alchemist(),
+    new assassin(),
+    new double(),
+    new templar(),
+    new summoner(),
+];
